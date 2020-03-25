8af13cd6ef5bea69ab2a1f44f8418827044fbc5a
chadcarlson <chad.carlson@platform.sh>
2020-03-25 14:42:14 -0400

    Remove chmod.

--- frontend/update_backendurl.sh
+++ frontend/update_backendurl.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-# PLATFORM_ROUTES=$( cat test.txt )
+PLATFORM_ROUTES=$( cat test.txt )
 
-export BACKEND_ID="wordpress"
-WP_BACKEND_URL=$( echo $PLATFORM_ROUTES | base64 --decode | jq '. | values[]' | jq --arg backend_id "$BACKEND_ID"  'if .id==$backend_id then .to else empty end' )
+# BACKEND_ID matched id attribute of the backend redirect
+BACKEND_ID="wordpress"
+# Export the backend's upstream URL
+export WP_BACKEND_URL=$( echo $PLATFORM_ROUTES | base64 --decode | jq '. | values[]' | jq --arg backend_id "$BACKEND_ID"  'if .id==$backend_id then .to else empty end' )
+
 echo $WP_BACKEND_URL
