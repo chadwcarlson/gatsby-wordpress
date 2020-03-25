3f0ea02bfc7798eea5f720a86b44f5026d674350
chadcarlson <chad.carlson@platform.sh>
2020-03-25 14:10:57 -0400

    just echo.

--- frontend/update_backendurl.sh
+++ frontend/update_backendurl.sh
@@ -2,4 +2,4 @@
 
 export BACKEND_ID="wordpress"
 WP_BACKEND_URL=$( echo $PLATFORM_ROUTES | base64 --decode | jq '. | values[]' | jq 'if .id==$ENV.BACKEND_ID then .to else empty end')
-export $WP_BACKEND_URL
+echo $WP_BACKEND_URL
