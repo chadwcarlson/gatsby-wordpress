16f1eb147c414037ddc03495d1d44e5c3d207fda
chadcarlson <chad.carlson@platform.sh>
2020-03-25 15:23:02 -0400

    Remove the baggage.

--- frontend/test.js
+++ frontend/test.js
@@ -1,3 +1,5 @@
 const config = require("platformsh-config").config();
 
-console.log(config.getRoute("wordpress").url);
+console.log(config.getRoute("wordpress").url.split("/")[2]);
+// var url = "https://backend.pr-1-djjnuwy-wkdydhxitndua.eu-3.platformsh.site/"
+// console.log(url.split("/")[2])
