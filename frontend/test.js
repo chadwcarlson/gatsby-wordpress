135537450ae2b8b929883a1abd5814543f26a051
chadcarlson <chad.carlson@platform.sh>
2020-03-25 15:44:30 -0400

    Probably the string format. Test it.

--- frontend/test.js
+++ frontend/test.js
@@ -1,3 +1,5 @@
 const config = require("platformsh-config").config();
 
 console.log(config.getRoute("wordpress").url.split("/")[2]);
+console.log(config.credentials('backend').host)
+console.log(config.credentials('backend').host.url.split("/")[2])
