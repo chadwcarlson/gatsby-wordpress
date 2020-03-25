4b00f77e344e046abbab6a8934c74548f835e57a
chadcarlson <chad.carlson@platform.sh>
2020-03-25 16:20:47 -0400

    Try local host config.

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -19,7 +19,8 @@ module.exports = {
          * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
          * Example : 'demo.wp-api.org' or 'www.example-site.com'
          */
-        baseUrl: "backend.pr-1-djjnuwy-wkdydhxitndua.eu-3.platformsh.site",
+        // baseUrl: "backend.pr-1-djjnuwy-wkdydhxitndua.eu-3.platformsh.site",
+        baseUrl: "backend.localhost"
         // baseUrl: `config.getRoute("wordpress").url.split("/")[2]`,
         // baseUrl: config.getRoute("wordpress").url.split("/")[2],
         // baseUrl: `${config.getRoute("wordpress").url.split("/")[2]}`,
