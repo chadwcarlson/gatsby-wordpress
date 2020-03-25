7c17a658d875640ff7551e71359c5f1cc7899345
chadcarlson <chad.carlson@platform.sh>
2020-03-25 14:34:05 -0400

    Comment out.

--- frontend/update_backendurl.sh
+++ frontend/update_backendurl.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-PLATFORM_ROUTES=$( cat test.txt )
+# PLATFORM_ROUTES=$( cat test.txt )
 
 export BACKEND_ID="wordpress"
 WP_BACKEND_URL=$( echo $PLATFORM_ROUTES | base64 --decode | jq '. | values[]' | jq --arg backend_id "$BACKEND_ID"  'if .id==$backend_id then .to else empty end' )
