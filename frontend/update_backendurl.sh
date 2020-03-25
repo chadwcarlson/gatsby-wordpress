71499d315ab9e647cd9071a7fd2cb28642f22b4a
chadcarlson <chad.carlson@platform.sh>
2020-03-25 14:47:05 -0400

    Add slash.

--- frontend/update_backendurl.sh
+++ frontend/update_backendurl.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-PLATFORM_ROUTES=$( cat test.txt )
+# PLATFORM_ROUTES=$( cat test.txt )
 
 # BACKEND_ID matched id attribute of the backend redirect
 BACKEND_ID="wordpress"
 # Export the backend's upstream URL
 export WP_BACKEND_URL=$( echo $PLATFORM_ROUTES | base64 --decode | jq '. | values[]' | jq --arg backend_id "$BACKEND_ID"  'if .id==$backend_id then .to else empty end' )
 
-echo $WP_BACKEND_URL
+# echo $WP_BACKEND_URL
