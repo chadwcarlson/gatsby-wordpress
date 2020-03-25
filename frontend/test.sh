65dc0d8de26cb89a1ed91c1c03325740f0ce0fc1
chadcarlson <chad.carlson@platform.sh>
2020-03-25 13:35:03 -0400

    Add id to redirect.

--- frontend/test.sh
+++ frontend/test.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-echo $PLATFORM_ROUTES | base64 --decode | jq 
+WP_BACKEND_URL="nothing"
+ROUTES=$( echo $PLATFORM_ROUTES | base64 --decode )
+echo $ROUTES | jq
+echo $ROUTES | jq '. | values[]' | jq 'if .id == "wordpress" then .to else "nope" end'
+echo $WP_BACKEND_URL
