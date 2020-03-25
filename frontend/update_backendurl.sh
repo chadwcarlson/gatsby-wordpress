a2c67460d7ce804bfa646d21f046fedebb0d9b3b
chadcarlson <chad.carlson@platform.sh>
2020-03-25 14:23:23 -0400

    Pass a var .

--- frontend/update_backendurl.sh
+++ frontend/update_backendurl.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+PLATFORM_ROUTES=$( cat test.txt )
+
 export BACKEND_ID="wordpress"
-WP_BACKEND_URL=$( echo $PLATFORM_ROUTES | base64 --decode | jq '. | values[]' | jq 'if .id==$ENV.BACKEND_ID then .to else empty end')
+WP_BACKEND_URL=$( echo $PLATFORM_ROUTES | base64 --decode | jq '. | values[]' | jq --arg backend_id "$BACKEND_ID"  'if .id==$backend_id then .to else empty end' )
 echo $WP_BACKEND_URL
